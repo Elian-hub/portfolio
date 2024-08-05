6d16b9bfecb018dde558188f0b303298b8519ad0
[Elian-hub] <[elianmars13@gmail.com]>
2024-08-06 00:16:34 +0300

    oi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-// import './App.css';
+import './App.css';
 import Header from './Components/Header/Header';
 import './scss/main.scss';
 import Intro from './Components/Intro/Intro';
