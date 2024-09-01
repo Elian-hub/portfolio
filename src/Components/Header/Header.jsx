2b93feb4c424268c91ce4d94841fc7f674a5e98c
[Elian-hub] <[elianmars13@gmail.com]>
2024-09-01 08:29:39 +0300

    latest

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaHome } from 'react-icons/fa';
 import { FcAbout, FcServices } from 'react-icons/fc';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Header.css';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='bg-gray-800 fixed top-0 left-0 w-full flex flex-col md:flex-row items-center md:items-center md:justify-between p-4'>
-      <div className='flex justify-center md:justify-start w-full md:w-auto mb-4 md:mb-0'>
-        <h1 className='text-2x text-white font-bold cursor-pointer'>
-          myPortfolio
-        </h1>
+      <div className='flex justify-between items-center w-full md:w-auto'>
+        <h1 className='text-2x text-white font-bold'>myPortfolio</h1>
+        <button
+          onClick={toggleMenu}
+          className='text-white text-2xl md:hidden focus:outline-none'
+        >
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </button>
       </div>
-      <ul className='text-white flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-4'>
-        <li>
+      <ul
+        className={`text-white flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-4 lg:inline-block transition-all duration-300 ease-in-out ${
+          isOpen ? 'block' : 'hidden md:block'
+        }`}
+      >
+        <li className='lg:inline-block'>
           <a href='/'>
-            <FaHome className='inline-block w-4 h-4 mr-1'></FaHome>
+            <FaHome className='inline-block w-5 h-5 mr-1'></FaHome>
             HOME
           </a>
         </li>
-        <li>
+        <li className='lg:inline-block'>
           <a href='/'>
-            <FcAbout className='inline-block w-4 h-4 mr-1'></FcAbout>
+            <FcAbout className='inline-block w-5 h-5 mr-1'></FcAbout>
             ABOUT ME
           </a>
         </li>
-        <li>
+        <li className='lg:inline-block'>
           <a href='/'>
-            <FcServices className='inline-block w-4 h-4 mr-1'></FcServices>
+            <FcServices className='inline-block w-5 h-5 mr-1'></FcServices>
             SERVICES
           </a>
         </li>
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -8,15 +8,13 @@ const Intro = () => {
     typeSpeed: 120,
     deleteSpeed: 70,
   });
+
   return (
-    <div>
-      <p>Hello human,</p>
-      <h1>
-        I'm a {''}
-        <span
-          className='text-2xl'
-          style={{ fontweight: 'bold', color: 'white' }}
-        >
+    <div className='flex flex-col items-center justify-center min-h-screen p-4 text-center md:text-left'>
+      <p className='text-lg md:text-xl mb-4'>Hello human,</p>
+      <h1 className='text-2xl md:text-3xl font-bold'>
+        I'm a{' '}
+        <span className='text-2xl text-white md:text-4xl font-bold'>
           {text}
         </span>
         <Cursor />
