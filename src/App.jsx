4873d7eb0a03bd7677a337c6e5aefecbcf54c8d7
[Elian-hub] <[elianmars13@gmail.com]>
2024-09-10 20:32:07 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import './scss/main.scss';
 import Intro from './Components/Intro/Intro';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay (e.g., fetching data or resources)
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Set loading to false after 2 seconds
+    }, 3000);
+
+    // Cleanup the timer in case of component unmounting
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='flex items-center justify-center min-h-screen text-white text-2xl'>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
