772f932660441ecf664851d2bc9f57501aa66fc8
[Elian-hub] <[elianmars13@gmail.com]>
2025-02-01 02:20:35 +0300

    links?

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, AppBar, Toolbar } from '@mui/material';
 import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 
 const Header = () => {
@@ -7,69 +7,81 @@ const Header = () => {
     'https://www.linkedin.com/in/elian-marube-2891a52bb?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app';
   const urlTwitter = 'https://x.com/wannaBeMase?t=paWjfK31TtNa79ijK0Jm8g&s=09';
   const urlGitHub = 'https://github.com/Elian-hub';
+
   return (
-    <Box display='flex' gap={2}>
-      <Box display='flex'>
-        <Typography variant='h5' fontWeight='bold' color='white'>
-          Elian Marube
-        </Typography>
-      </Box>
-      <Box display='flex' gap={2}>
-        <Link to='/'>
-          <Typography
-            variant='h6'
-            color='white'
-            sx={{
-              '&:hover': {
-                color: 'black',
-                cursor: 'pointer',
-              },
-            }}
-          >
-            Home
+    <>
+      <AppBar
+        position='sticky'
+        sx={{ background: 'transparent', display: 'flex' }}
+      >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant='h5' fontWeight='bold' color='white'>
+            Elian Marube
           </Typography>
-        </Link>
-        <Link to='/about'>
-          <Typography
-            variant='h6'
-            color='white'
-            sx={{
-              '&:hover': {
-                color: 'black',
-                cursor: 'pointer',
-              },
-            }}
+
+          <Box
+            display='flex'
+            gap={2}
+            sx={{ flexGrow: 1, justifyContent: 'center' }}
           >
-            About
-          </Typography>
-        </Link>
-        <Link to='/services'>
-          <Typography
-            variant='h6'
-            color='white'
-            sx={{
-              '&:hover': {
-                color: 'black',
-                cursor: 'pointer',
-              },
-            }}
-          >
-            Services
-          </Typography>
-        </Link>
-      </Box>
-      <Box display='flex' gap={2}>
-        <a href={urlGitHub} target='blank'>
-          <FaGithub />
-        </a>
-        <a href={urlLinkedIn} target='blank'>
-          <FaLinkedin />
-        </a>
-        <a href={urlTwitter} target='blank'>
-          <FaTwitter />
-        </a>
-      </Box>
-    </Box>
+            <Link to='/'>
+              <Typography
+                variant='h6'
+                color='white'
+                sx={{
+                  '&:hover': {
+                    color: 'black',
+                    cursor: 'pointer',
+                  },
+                }}
+              >
+                Home
+              </Typography>
+            </Link>
+            <Link to='/about'>
+              <Typography
+                variant='h6'
+                color='white'
+                sx={{
+                  '&:hover': {
+                    color: 'black',
+                    cursor: 'pointer',
+                  },
+                }}
+              >
+                About
+              </Typography>
+            </Link>
+            <Link to='/services'>
+              <Typography
+                variant='h6'
+                color='white'
+                sx={{
+                  '&:hover': {
+                    color: 'black',
+                    cursor: 'pointer',
+                  },
+                }}
+              >
+                Services
+              </Typography>
+            </Link>
+          </Box>
+
+          <Box sx={{ display: 'flex' }} gap={2}>
+            <a href={urlGitHub} target='blank'>
+              <FaGithub color='white' />
+            </a>
+            <a href={urlLinkedIn} target='blank'>
+              <FaLinkedin color='white' />
+            </a>
+            <a href={urlTwitter} target='blank'>
+              <FaTwitter color='white' />
+            </a>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
